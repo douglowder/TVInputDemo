eac88154142b3d5e6abbc54feed29555f3b624c1
Douglas Lowder <douglowder@mac.com>
2023-01-15 13:53:11 -0800

    Changes for RNTV 0.69.6-2

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  env: {
-    production: {
-      plugins: ['react-native-paper/babel'],
-    },
-  },
+  plugins: [
+    'react-native-reanimated/plugin',
+    '@babel/plugin-proposal-export-namespace-from',
+    'react-native-paper/babel', 
+  ],
 };
